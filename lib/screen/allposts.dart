cec0ac1f7f4afc622def53cc78e132a116d8548b
AmreetPoudel <AmreetPoudel>
2022-05-18 21:01:16 +0545

    feed done in test file (allposts.dart) need refactor and manage the code to different file

--- lib/screen/allposts.dart
+++ lib/screen/allposts.dart
@@ -46,17 +46,19 @@ class _allPostsState extends State<allPosts> {
             item['uid'] = document.id;
             // print(storedocs);
           }).toList();
+          //here we have all items in the list i.e all blood group on bloodgroup and similarly for district post and phone number
+          //tried this because if kei add garnu paryo vane simply copy garara thorai change garda hunxa
 
           List bloodGroup = storedocs.map((i) => i['bloodGroup']).toList();
           List district = storedocs.map((i) => i['district']).toList();
           List post = storedocs.map((i) => i['post']).toList();
-          List phoneNumber = storedocs.map((i) => i['phoneNumber']).toList();
+          List phoneNo = storedocs.map((i) => i['phoneNo']).toList();
           return SafeArea(
             child: Container(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+              padding: EdgeInsets.all(5),
               child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
                 child: Column(
@@ -66,7 +68,7 @@ class _allPostsState extends State<allPosts> {
                         width: double.infinity,
                         child: Card(
                           margin: EdgeInsets.symmetric(
-                              horizontal: 10.0, vertical: 20.0),
+                              horizontal: 5.0, vertical: 11.0),
                           elevation: 7,
                           shadowColor: Colors.red,
                           borderOnForeground: true,
@@ -102,7 +104,7 @@ class _allPostsState extends State<allPosts> {
                                 ),
                               ),
                               Text(
-                                'phone Number: ${phoneNumber[i]}',
+                                'phone Number: ${phoneNo[i]}',
                                 style: TextStyle(
                                   color: Colors.red.shade500,
                                   fontSize: 18.0,
